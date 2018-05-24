eaef1bbd862e77dcbe6bb244b65db571ffd1724a
SuperH <SuperHuJ@163.com>
2018-05-25 01:40:55 +0800

    Mysql和redis集成

--- manage.py
+++ manage.py
@@ -1,18 +1,36 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from redis import StrictRedis
 
 class Config(object):
     """配置文件的加载"""
     # 开启调试模式
     DEBUG = True
+    # 配置Mysql数据库的连接信息
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/information_29'
+    # 不追踪数据库的修改节省内存
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # 配置redis数据库
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
 
 app = Flask(__name__)
 
 # 获取配置信息
 app.config.from_object(Config)
 
+# 创建连接到mysql数据库的对象
+db = SQLAlchemy(app)
+
+# 创建连接到reids数据库对象
+redis_store = StrictRedis(host=Config.REDIS_HOST,port=Config.REDIS_PORT)
+
 @app.route('/')
 def index():
-    return index
+    # 测试redis数据库
+    redis_store.set('name','itheima')
+
+    return 'index'
 
 if __name__ == '__main__':
     app.run()
