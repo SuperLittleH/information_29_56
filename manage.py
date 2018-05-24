8c74f027c8c402d2f138aa308bfa3b62152b6e98
SuperH <SuperHuJ@163.com>
2018-05-25 01:23:01 +0800

    配置文件的加载

--- manage.py
+++ manage.py
@@ -1,9 +1,14 @@
 from flask import Flask
 
+class Config(object):
+    """配置文件的加载"""
+    # 开启调试模式
+    DEBUG = True
 
 app = Flask(__name__)
 
-
+# 获取配置信息
+app.config.from_object(Config)
 
 @app.route('/')
 def index():
