a9f9bac3e1484f64ac3c910d662d26f391640e16
SuperH <SuperHuJ@163.com>
2018-05-27 21:49:41 +0800

    title左图标

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,8 +1,11 @@
 from . import index_blue
-from flask import render_template
+from flask import render_template,current_app
 
 @index_blue.route('/')
 def index():
     """主页"""
-
     return render_template('news/index.html')
+
+@index_blue.route('/favicon.ico',methods=['GET'])
+def favicon():
+    return render_template()
