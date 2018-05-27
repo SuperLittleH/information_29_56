19ec4300f3f920bb194ea5dfd0801b72585e72c4
SuperH <SuperHuJ@163.com>
2018-05-27 21:32:05 +0800

    渲染主页

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,8 +1,8 @@
 from . import index_blue
-from info import redis_store
+from flask import render_template
 
 @index_blue.route('/')
 def index():
     """主页"""
-    redis_store.set('name','zxc')
-    return "index"
+
+    return render_template('news/index.html')
