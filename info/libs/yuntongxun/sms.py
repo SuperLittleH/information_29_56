f698a710ea417736546469e66ec2c3514356c793
SuperH <SuperHuJ@163.com>
2018-05-28 09:45:03 +0800

    云平台验证

--- info/libs/yuntongxun/sms.py
+++ info/libs/yuntongxun/sms.py
@@ -60,6 +60,7 @@ class CCP(object):
         # @param datas 内容数据 格式为数组 例如：{'12','34'}，如不需替换请填 ''
         # @param temp_id 模板Id
         result = self.rest.sendTemplateSMS(to, datas, temp_id)
+        print(result)
         # 如果云通讯发送短信成功，返回的字典数据result中statuCode字段的值为"000000"
         if result.get("statusCode") == "000000":
             # 返回0 表示发送短信成功
@@ -72,4 +73,4 @@ class CCP(object):
 if __name__ == '__main__':
     ccp = CCP()
     # 注意： 测试的短信模板编号为1
-    ccp.send_template_sms('your mobile number', ['666', 5], 1)
+    ccp.send_template_sms('15971306907', ['666', 5], 1)
