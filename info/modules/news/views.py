0f5d3839303572bf20fec5c7843a1da62084049e
SuperH <SuperHuJ@163.com>
2018-06-01 12:25:13 +0800

    新闻收藏之收藏和取消收藏后端逻辑实现

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,16 +1,55 @@
 # 新闻详情
-from flask import  render_template,session,current_app,abort,g
+from flask import  render_template,session,current_app,abort,g,jsonify,request
 from . import news_blue
-from info.models import User,News,db
-from info import constants
+from info.models import User,News
+from info import constants,db,response_code
 from info.utils.comment import user_login_data
 
 
-@news_blue.route('/news/collect')
+@news_blue.route('/news_collect',methods=['POST'])
 @user_login_data
 def news_collect():
     """新闻收藏"""
-    pass
+    # 1.获取登录用户信息
+    user = g.user
+    if not user:
+        return jsonify(errno=response_code.RET.SESSIONERR,errmsg='用户未登录')
+
+    # 2.接受参数
+    news_id = request.json.get('news_id')
+    action = request.json.get('action')
+
+    # 3.校验参数
+    if not all([news_id,action]):
+        return jsonify(errno=response_code.RET.PARAMERR,errmsg='缺少参数')
+    if action not in ['collect','cancel_collect']:
+        return jsonify(errno=response_code.RET.PARAMERR,errmsg='参数错误')
+
+    # 4.查询新闻信息
+    try:
+        news = News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=response_code.RET.DBERR,errmsg='查询新闻数据失败')
+    if not news:
+        return jsonify(errno=response_code.RET.NODATA,errmsg='新闻数据不存在')
+
+    # 5.收藏和取消收藏新闻
+    if action == 'collect':
+        if news not in user.collection_news:
+            user.collection_news.append(news)
+    else:
+        if news in user.collection_news:
+            user.collection_news.remove(news)
+    try:
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        db.session.rollback()
+        return jsonify(errno=response_code.RET.DBERR,errmsg='收藏操作失败')
+
+    # 6.响应操作结果
+    return jsonify(errno=response_code.RET.OK,errmsg='操作成功')
 
 @news_blue.route('/detail/<int:news_id>')
 @user_login_data
