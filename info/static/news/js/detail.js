66e0398bb289bbd0ef382a02b240838b8359ff89
SuperH <SuperHuJ@163.com>
2018-06-02 23:57:51 +0800

    前端更新新闻评论条数

--- info/static/news/js/detail.js
+++ info/static/news/js/detail.js
@@ -127,6 +127,10 @@ $(function(){
                     $('.comment_sub').blur();
                     // 清空输入框内容
                     $(".comment_input").val("")
+
+                    // 更新评论条数
+                    updateCommentCount()
+
                 }else {
                     alert(resp.errmsg)
                 }
@@ -224,6 +228,10 @@ $(function(){
                         $this.prev().val('')
                         // 关闭
                         $this.parent().hide()
+
+                        // 更新评论条数
+                        updateCommentCount()
+
                     }else {
                         alert(resp.errmsg)
                     }
@@ -242,3 +250,9 @@ $(function(){
 
     })
 })
+
+// 更新评论条数
+function updateCommentCount() {
+    var count = $(".comment_list").length;
+    $(".comment_count").html(count + "条评论");
+}
