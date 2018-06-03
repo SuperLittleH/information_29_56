ca1d4d077e51a45985f1c7fc4af3c6fcb5a90a56
SuperH <SuperHuJ@163.com>
2018-06-03 16:53:46 +0800

    展示user.html模板 + 点击用户名跳转到user.html

--- info/modules/user/views.py
+++ info/modules/user/views.py
@@ -1,8 +1,21 @@
 # 个人中心
 from . import user_blue
-from flask import  render_template
+from flask import  render_template,g,redirect,url_for
+from info.utils.comment import user_login_data
 
 @user_blue.route('/info')
+@user_login_data
 def user_info():
     """个人中心入口"""
-    return render_template('news/user.html')
+    # 提示:必须是登录用户才能进入
+
+    # 获取登录用户信息
+    user = g.user
+    if not user:
+        return redirect(url_for('index.index'))
+
+    context = {
+        'user':user
+    }
+
+    return render_template('news/user.html',context=context)
