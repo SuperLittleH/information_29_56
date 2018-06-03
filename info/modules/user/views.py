76be441fb96b37d90be5bf226c42e1441648093c
SuperH <SuperHuJ@163.com>
2018-06-03 17:20:16 +0800

    基本资料查询和展示

--- info/modules/user/views.py
+++ info/modules/user/views.py
@@ -3,6 +3,20 @@ from . import user_blue
 from flask import  render_template,g,redirect,url_for
 from info.utils.comment import user_login_data
 
+@user_blue.route('/base_info')
+@user_login_data
+def base_info():
+    """基本资料"""
+    #获取登录用户信息
+    user = g.user
+    if not user:
+        return redirect(url_for('index.index'))
+
+    context = {
+        'user':user
+    }
+    return render_template('news/user_base_info.html',context=context)
+
 @user_blue.route('/info')
 @user_login_data
 def user_info():
