7dbdfeeda4bf1c6b3fa576ba72f8710e09406006
SuperH <SuperHuJ@163.com>
2018-06-03 17:58:16 +0800

    个人中心之基本资料修改后端逻辑

--- info/modules/user/views.py
+++ info/modules/user/views.py
@@ -1,21 +1,57 @@
 # 个人中心
 from . import user_blue
-from flask import  render_template,g,redirect,url_for
+from flask import  render_template,g,redirect,url_for,request,jsonify,current_app,session
 from info.utils.comment import user_login_data
+from info import response_code,db
 
-@user_blue.route('/base_info')
+@user_blue.route('/base_info',methods=['GET','POST'])
 @user_login_data
 def base_info():
     """基本资料"""
-    #获取登录用户信息
+
+    #1.获取登录用户信息
     user = g.user
     if not user:
         return redirect(url_for('index.index'))
 
-    context = {
-        'user':user
-    }
-    return render_template('news/user_base_info.html',context=context)
+    # 2.实现GET请求的逻辑
+    if request.method == "GET":
+        # 构造渲染数据的上下文
+        context = {
+            'user':user
+        }
+        # 渲染界面
+        return render_template('news/user_base_info.html',context=context)
+
+    # 3.POST请求逻辑
+    if request.method == "POST":
+        # 3.1获取参数
+        nick_name = request.json.get('nick_name')
+        signature = request.json.get('signature')
+        gender = request.json.get('gender')
+
+        # 3.2校验参数
+        if not all([nick_name,signature,gender]):
+            return jsonify(errno=response_code.RET.PARAMERR, errmsg="缺少参数")
+        if gender not in ['MAN','WOMAN']:
+            return jsonify(errno=response_code.RET.PARAMERR, errmsg="参数错误")
+
+        # 3.3修改用户的基本资料
+        user.signature = signature
+        user.nick_name = nick_name
+        user.gender = gender
+
+        try:
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            current_app.logger.error(e)
+
+        # 4.注意:修改了昵称以后记得将状态保持中的昵称也修改
+        session['nick_name'] = nick_name
+
+        # 5.响应修改结果
+        return jsonify(errno=response_code.RET.OK, errmsg="修改基本资料成功")
 
 @user_blue.route('/info')
 @user_login_data
